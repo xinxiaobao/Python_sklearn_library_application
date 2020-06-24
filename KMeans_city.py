dc89f2102ea44ae55666c3a85ba457381c13b1e7
xinxiaobao <18030179713@163.com>
2020-06-24 16:16:46 +0800

    update Kmeans city

--- KMeans_city.py
+++ KMeans_city.py
@@ -15,7 +15,22 @@ def loadData(filePath):
 
 
 
-# data_path = './city.txt'
-data_path = '..//课程数据/聚类/31省市居民家庭消费水平-city.txt'
+data_path = '../课程数据/聚类/31省市居民家庭消费水平-city.txt'
 data, cityName = loadData(data_path)
-print(len(data))
+n_clusters = int(input('请输入需要聚类数量：'))
+km = KMeans(n_clusters)
+
+label = km.fit_predict(data)
+expenses = np.sum(km.cluster_centers_, axis=1)
+
+CityCluster = ()
+for i in range(n_clusters):
+    CityCluster += ([],)
+
+
+for i in range(len(cityName)):
+    CityCluster[label[i]].append(cityName[i])
+
+for i in range(len(CityCluster)):
+    print('Expenses: %.2f'%expenses[i])
+    print(CityCluster[i])
